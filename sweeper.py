c975f55d9414631406f826ce6460800aa405a44b
anjinsan43 <capacitor2.7182@gmail.com>
2017-03-10 15:08:01 -0600

    Add files via upload

--- sweeper.py
+++ sweeper.py
@@ -1,15 +1,32 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
 
-root = Tk()
-root.title("Mine Sweeper")
+
+
+def draw_mine_field(x=5, y=3):
+
+	mine_frame = tk.Toplevel(root)
+	mine_frame.grid()
+
+	for b in range(y):
+		for a in range(x):
+			ttk.Button(mine_frame, text='*', width=3 ).grid(column=a, row=b)
+
+
+root = tk.Tk()
+root.title("MS")
 
 mainframe = ttk.Frame(root)
 
+ttk.Label(root,text="y").grid(row=1,column=1)
+y_entry_box = ttk.Entry(root).grid(row=1,column=2)
 
-for y in range(5):
-	for x in range(5):
-		ttk.Button(root, text='*', width=3 ).grid(column=x, row=y)
+ttk.Label(root,text="x").grid(row=1,column=3)
+x_entry_box = ttk.Entry(root).grid(row=1,column=4)
+
+ttk.Button(root,text="Start",command=draw_mine_field).grid(row=2,column=1)
+ttk.Button(root,text="Quit",command=root.destroy).grid(row=2,column=2)
+
 
 root.mainloop()
