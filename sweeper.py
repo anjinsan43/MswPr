a852879432801abc9dbf9d115d04a20cd449b5f0
anjinsan43 <capacitor2.7182@gmail.com>
2017-03-23 15:22:50 -0500

    Add files via upload
    Experimenting with ways to store and retreive (and setup) the grid of squares.

--- sweeper.py
+++ sweeper.py
@@ -5,40 +5,60 @@ from tkinter import ttk
 import random
 
 
+class Square(self, bnt=ttk.Button, x,y):
+""" container class for each square """
+	def __init__(self):
+		self.mine_yn = False
+		self.flag_yn = False
+		self.prox_num = 0  #parser() will change this.
+		self.x = x
+		self.y = y
+		self.button = bnt
+		
+
 def draw_mine_field():
-	global sqr_list
-	global mine_frame
-	
-	sqr_list = []
-	
-	mine_frame = tk.Toplevel(root)
-	mine_frame.grid()
-	#user hit 'X' to close window.
-	mine_frame.protocol("WM_DELETE_WINDOW", mine_frame_close)
-	
-	root.withdraw()
+    global sqr_list
+    global mine_frame
+    
+    sqr_list = []
+    
+    mine_frame = tk.Toplevel(root)
+    mine_frame.grid()
+    #user hit 'X' to close window.
+    mine_frame.protocol("WM_DELETE_WINDOW", mine_frame_close)
+    
+    root.withdraw()
 
-	for a in range(int(x_str.get() )):
-		for b in range(int(y_str.get() )):
-			sqr_list.append(ttk.Button(mine_frame, text='[]', width=3 ).grid(column=a, row=b))
+	# create grid of squares (buttons)
+    for a in range(int(x_str.get() )):
+        for b in range(int(y_str.get() )):
+            sqr_list.append(object)
+            btn_tmp = ttk.Button(mine_frame, text='[]', width=3 )
+            
+			sqr_list[-1].grid(column=a, row=b)
 
+
+def parse_mines:
+
+	for i in 
+			
 			
 def root_close():
-	if tkMessageBox.askokcancel("Quit", "Do you want to quit?"):
-		root.destroy()
+    if tkMessageBox.askokcancel("Quit", "Do you want to quit?"):
+        root.destroy()
 
 def mine_frame_close():
-	root.deiconify()  # un-withdraw root
-	mine_frame.destroy()
+    root.deiconify()  # un-withdraw root
+    mine_frame.destroy()
 
-	
+    
 root = tk.Tk()
 root.title("MS")
 
 x_str = tk.StringVar()
-x_str.set('4')
+x_str.set('6')
 y_str = tk.StringVar()
-y_str.set('2')
+y_str.set('4')
 
 mines_pct_str = tk.StringVar()
 mines_pct_str.set('20')
@@ -73,4 +93,3 @@ quit_button.pack() #grid(row=2,column=2)
 root.protocol("WM_DELETE_WINDOW", root_close)
 
 root.mainloop()
-print(sqr_list)
