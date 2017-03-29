4eb3d09c80371d8bcb08409b67dbf04e83c77d99
anjinsan43 <capacitor2.7182@gmail.com>
2017-03-29 15:09:18 -0500

    Add files via upload
    working on parse_mines()... stuck on bug that clears all but the first key in sqr_dict.

--- sweeper.py
+++ sweeper.py
@@ -10,12 +10,11 @@ class Square(object):
     def __init__(self):
         self.mine_yn = False
         self.flag_yn = False
-        self.prox_num = 0   # parser() will change this.
+        self.prox_num = 0   # number of nearby mines, parser() will fill this in.
         self.button = None  # ttk.Button instance.
         
 
 def create_mine_field():
-    global sqr_list
     global mine_frame
     global sqr_dict
     
@@ -33,43 +32,62 @@ def create_mine_field():
     for x in range(int(x_str.get() )):
         for y in range(int(y_str.get() )):
             coord = 'x'+str(x) + 'y'+str(y)
-            sqr_dict[coord] = Square #sqr_list.append(Square)
-            #sqr_list[-1].x = x
-            #sqr_list[-1].y = y
-            
+            sqr_dict[coord] = Square 
+            #print('coord='+coord) #debug
             #populate with mines
             if ( rand()*100 < int(mines_pct_str.get()) ):
-                sqr_dict[coord].mine_yn = True #sqr_list[-1].mine_yn = True
+                sqr_dict[coord].mine_yn = True 
             else:
-                sqr_dict[coord].mine_yn = False #sqr_list[-1].mine_yn = False
+                sqr_dict[coord].mine_yn = False 
 
             # draw boxes
-            if sqr_dict[coord].mine_yn:  #sqr_list[-1].mine_yn:
+            if sqr_dict[coord].mine_yn:  
                 t = '*'
             else: t = ' '
             
-            #sqr_list[-1].button = ttk.Button(mine_frame, text=t, width=3 )
-            #sqr_list[-1].button.grid(column=x, row=y)
             sqr_dict[coord].button = ttk.Button(mine_frame, text=t, width=3 )
             sqr_dict[coord].button.grid(column=x, row=y)
             # done, next: parse!
+            print('create_mines, sqr_dict='+str(sqr_dict))
+            #mine_frame.update() #???
+            parse_mines()
 
 def parse_mines():
+    global sqr_dict
+    print('parse_mines, sqr_dict='+str(sqr_dict))
+    #x=1  #debug
+    #y=1  #debug
     #loop over dictionary keys(coordinates)
         #look at the 8 adjacent squares
         #catch NameError for edges of board
-    def try_a_sqare(sq): #sq = Square instance
+    def try_a_square(sq): #sq = coordinate string(key)
         try:
-            if sqr_dict[sq].mine_yn = True:  return 1
-            if sqr_dict[sq].mine_yn = False: return 0
-        except NameError:
+            if sqr_dict[sq].mine_yn == True:  return 1
+            if sqr_dict[sq].mine_yn == False: return 0
+        except KeyError:
+            print('KeyError for '+sq)
             return 0
+           
             
+    n = 1 #after debugging change back to 0 when done
+    for x in range(int(x_str.get() )):
+        for y in range(int(y_str.get() )):
+            n = n + try_a_square('x'+str(x+1)+'y'+str(y+1))
+            n = n + try_a_square('x'+str(x+1)+'y'+str(y  ))
+            n = n + try_a_square('x'+str(x+1)+'y'+str(y-1))
+            n = n + try_a_square('x'+str(x  )+'y'+str(y+1))
+            n = n + try_a_square('x'+str(x  )+'y'+str(y-1))
+            n = n + try_a_square('x'+str(x-1)+'y'+str(y+1))
+            n = n + try_a_square('x'+str(x-1)+'y'+str(y  ))
+            n = n + try_a_square('x'+str(x-1)+'y'+str(y-1))
+            if sqr_dict['x'+str(x)+'y'+str(y)].mine_yn == False: pass
+                #sqr_dict['x'+str(x)+'y'+str(y)].prox_num = n
+            print('n='+str(n))
+            #print('x'+str(x)+'y'+str(y)+': '+str(n)) #(debug) print n for each sq
+            #sqr_dict['x'+str(x)+'y'+str(y)].button.text=(str(n)) #(debug) show n on each button. (not working) BUG HERE!
+            #root.update()
+        
 
-    for i in sqr_dict:  # i is a key/string
-		#use regex for splitting x,y
-		
-		
 def root_close():
     if tkMessageBox.askokcancel("Quit", "Do you want to quit?"):
         root.destroy()
